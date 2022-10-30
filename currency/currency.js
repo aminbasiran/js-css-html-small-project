477be016b47a9a058277df3fe8c847af1ad977fe
aminpls <111835474+aminbasiran@users.noreply.github.com>
2022-10-30 16:18:26 +0800

    Update currency.js

--- currency/currency.js
+++ currency/currency.js
@@ -1,15 +1,4 @@
-// console.log("helloe")
 
-// const getData = (initCountry) => {
-//     fetchresult = fetch(`https://v6.exchangerate-api.com/v6/dd3ddd6a10ff70f35aecfb12/latest/${initCountry}`);
-//     fetchresult
-//     .then(res => res.json())
-//     .then(d => {
-//             console.log(d.conversion_rates)
-//         })
-// } 
-
-// getData("USD")
 const axios = require("axios");
 const getData = async (initCurrency,finalCurrency) => {
     const response = await axios.get(`https://v6.exchangerate-api.com/v6/dd3ddd6a10ff70f35aecfb12/latest/${initCurrency}`);
@@ -26,4 +15,3 @@ const convertValue = async (initCurrency,finalCurrency ,amount) =>{
 }
 
 convertValue("USD","MYR",2).to
-// getData("USD","EUR")

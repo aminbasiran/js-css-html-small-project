6b00b7c45591f4ab3aaae96ac94cfa7b9fed2303
Muhammad Amin <work.aminbasiran@gmail.com>
2022-10-30 16:15:42 +0800

    first version converter

--- currency/currency.js
+++ currency/currency.js
@@ -1,1 +1,29 @@
-pushing to exs repo
+// console.log("helloe")
+
+// const getData = (initCountry) => {
+//     fetchresult = fetch(`https://v6.exchangerate-api.com/v6/dd3ddd6a10ff70f35aecfb12/latest/${initCountry}`);
+//     fetchresult
+//     .then(res => res.json())
+//     .then(d => {
+//             console.log(d.conversion_rates)
+//         })
+// } 
+
+// getData("USD")
+const axios = require("axios");
+const getData = async (initCurrency,finalCurrency) => {
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/dd3ddd6a10ff70f35aecfb12/latest/${initCurrency}`);
+    const rate = response.data.conversion_rates[finalCurrency]
+    console.log(rate)
+    return rate
+}
+
+const convertValue = async (initCurrency,finalCurrency ,amount) =>{
+    const waitgetData = await getData(initCurrency, finalCurrency);
+    const convertAmount = (waitgetData * amount).toFixed(2);
+    console.log(convertAmount)
+
+}
+
+convertValue("USD","MYR",2).to
+// getData("USD","EUR")
